353b147495434a7c2e4384ee80968459783fbe2e
Veronica Lopez <nicaa0695@gmail.com>
2019-07-20 02:37:27 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,3 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-      }, 
-    :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-      }
-  }
+  
 end
