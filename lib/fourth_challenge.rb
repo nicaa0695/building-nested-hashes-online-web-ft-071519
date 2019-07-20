4212c61fcfe9e4270fcefc857e3474b3451efa51
Veronica Lopez <nicaa0695@gmail.com>
2019-07-20 02:51:41 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,7 +4,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [:name => "Benvolio", :age => "17", :attitude => "worried"
+      :hero_friends => [{:name => "Benvolio", :age => "17", :attitude => "worried"}, {
         ]
    }, 
    :capulet => {
@@ -12,7 +12,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        :name => "Mercutio", :age => "18", :attitude "hot-headed"
+        
       ]
    }
 }
